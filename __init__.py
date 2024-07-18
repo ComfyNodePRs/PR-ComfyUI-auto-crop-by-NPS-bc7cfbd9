c4bc20a2f9b84cebd06daeb8bda9283f8b338688
WX-NPS1598 <41763310@qq.com>
2024-07-18 17:32:45 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .auto_crop_by_NPS import auto crop by NPS
+NODE_CLASS_MAPPINGS = {
+    "AutoCropByNPS": AutoCropByNPS
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AutoCropByNPS": "auto crop by NPS"
+}
